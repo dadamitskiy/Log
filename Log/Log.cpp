03f4ee73a46a901b51bcfee67daeec358f943539
Daniel Adamitskiy <dadamitskiy@gmail.com>
2015-10-21 18:22:44 -0400

    Added MIT license comment header to all files. Also, reworked the Log class into a namespace due to the fact that no data members are needed for the Log class.

--- Log/Log.cpp
+++ Log/Log.cpp
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Daniel Adamitskiy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include "Log.h"
 #include <cstdio>
@@ -5,12 +28,6 @@
 #include <Windows.h>
 #include <direct.h>
 #include <fstream>
-
-Log* Log::GetInstance()
-{
-	static Log log;
-	return &log;
-}
 
 void Log::Print(const char* File, const char* Function, int LineNumber, const char* LogCategory, Verbosity::Type VerbosityLevel, OutputMethod::Type OutMethod, DetailLevel::Type Detail, const char* Format, ...)
 {
--- Log/Log.h
+++ Log/Log.h
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Daniel Adamitskiy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #ifndef _LOG_H_
 #define _LOG_H_
@@ -6,56 +29,55 @@
 #include <stdio.h>
 #include <crtdefs.h>
 
-#define LOG(Category, VerbosityLevel, OutputType, Detail, Format, ...) \
-	Log::GetInstance()->Print(__FILE__, __FUNCTION__, __LINE__, #Category, Verbosity::##VerbosityLevel, OutputMethod::##OutputType, DetailLevel::##Detail, Format, __VA_ARGS__);
-
-namespace Verbosity
-{
-	enum Type
-	{
-		Default,
-		Debug,
-		Warning,
-		Error,
-	};
-}
-
-namespace OutputMethod
-{
-	enum Type
-	{
-		All,
-		ConsoleWindow,
-		OutputWindow,
-		TextFile,
-	};
-}
-
-namespace DetailLevel
-{
-	enum Type
-	{
-		Low,
-		Medium,
-		High,
-	};
-}
+#define LOG(Category, VerbosityLevel, OutputType, Detail, Format, ...) Log::Print(__FILE__, __FUNCTION__, __LINE__, #Category, Log::Verbosity::##VerbosityLevel, Log::OutputMethod::##OutputType, Log::DetailLevel::##Detail, Format, __VA_ARGS__);
 
 /*
- * 
+ * A namespace containing the functions needed to log information to a console window, text file, or an output 
+ * window. A namespace is used over a struct or class because there are no data members to store in a Log object. 
  */
-class Log
+namespace Log
 {
-public:
+	/* Verbosity levels for logging messages with varying importance. */
+	namespace Verbosity
+	{
+		enum Type
+		{
+			Default,
+			Debug,
+			Warning,
+			Error,
+		};
+	}
 	
-	/* Singleton implementation. */
-	static Log* GetInstance();
+	/* Different destinations to output to. */
+	namespace OutputMethod
+	{
+		enum Type
+		{
+			All,
+			ConsoleWindow,
+			OutputWindow,
+			TextFile,
+		};
+	}
+	
+	/* Varying levels of detail included in a log statement. */
+	namespace DetailLevel
+	{
+		enum Type
+		{
+			Low,
+			Medium,
+			High,
+		};
+	}
 
 	/* Print the desired message to the desired location in the desired detail. */
 	void Print(const char* File, const char* Function, int LineNumber, const char* LogCategory, Verbosity::Type VerbosityLevel, OutputMethod::Type OutMethod, DetailLevel::Type Detail, const char* Format, ...);
 
 	/* Print a timestamp in the following order: Year.Month.Day-Hour:Minute:Second */
 	void PrintTimeStamp();
+
 	/* Output a timestamp to Visual Studio output window. */
 	void OutputTimeStamp();
 
@@ -64,11 +86,6 @@ public:
 
 	/* Reset the text color to white. */
 	void ResetTextColor();
-
-private:
-
-	Log() = default;
-	~Log() = default;
-};
+}
 
 #endif
